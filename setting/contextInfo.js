88121643754c4b7eb5b9c1548093a1d59072e76f
KAYA <harveykaya1@gmail.com>
2026-07-12 10:02:50 +0200

    Add files via upload

--- setting/contextInfo.js
+++ setting/contextInfo.js
@@ -10,21 +10,15 @@ const newsletters = [
 ];
 
 export function getContextInfo() {
-  try {
-    const newsletter = newsletters[Math.floor(Math.random() * newsletters.length)];
+  const newsletter = newsletters[Math.floor(Math.random() * newsletters.length)];
 
-    return {
-      forwardingScore: 999,
-      isForwarded: true,
-      forwardedNewsletterMessageInfo: {
-        newsletterJid: newsletter.jid,
-        newsletterName: newsletter.name,
-        serverMessageId: 150
-      }
-    };
-  } catch (error) {
-    console.error("Erreur dans getContextInfo:", error);
-    // Retourne un objet vide en cas d'erreur pour éviter que le bot ne crash
-    return {};
-  }
+  return {
+    forwardingScore: 999,
+    isForwarded: true,
+    forwardedNewsletterMessageInfo: {
+      newsletterJid: newsletter.jid,
+      newsletterName: newsletter.name,
+      serverMessageId: 150
+    }
+  };
 }
