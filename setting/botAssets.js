dde76eab3036edd0109508a1b2e6ef58f78f159c
KAYA <harveykaya1@gmail.com>
2026-09-16 19:27:16 +0200

    Add files via upload

--- setting/botAssets.js
+++ setting/botAssets.js
@@ -7,7 +7,7 @@ import { getSetting } from '../setting.js';
 export const BOT_VERSION = '1';
 export const BOT_SLOGAN = ' `『by ƘƛƳƛ ƁƠƬ』` ';
 
-const defaultGlobalImage = 'https://files.catbox.moe/lo0p98.png';
+const defaultGlobalImage = 'https://files.catbox.moe/go7kyn.png';
 export const DEFAULT_BOT_NAME = 'ƘƛƳƛ ƁƠƬ';
 
 const settingDir = path.join(process.cwd(), 'setting');
